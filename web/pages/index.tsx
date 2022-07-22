b55e18355fa6708a0a68ad871759b8e4d980fa9d
Qiushi Pan <qiu.gits@gmail.com>
2022-07-23 00:56:59 +0900

    chore: eslint monorepo settings for vscode

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -3,7 +3,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 import styles from 'styles/Home.module.css'
-
 import { VERSION } from '~/lib/version'
 
 const Home: NextPage = () => {
