86d225fc809f513c84fec8b05b16161093a87d0a
Qiushi Pan <17402261+qqpann@users.noreply.github.com>
2023-04-23 13:13:26 +0900

    add: page content

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,17 @@
 export default function Page() {
   return (
-    <div>
-      <h1>Page</h1>
+    <div style={{ padding: '4rem 5rem' }}>
+      <h1>{"qpan's Next.js template"}</h1>
+      <p>
+        {
+          'This is a template for Next.js. It includes TypeScript, ESLint, Prettier, and monorepo.'
+        }
+      </p>
+      <p>
+        <a href="https://github.com/qqpann/nextjs-template">
+          👉Fork me on <strong>GitHub</strong>
+        </a>
+      </p>
     </div>
   )
 }
